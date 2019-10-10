5ff7b81ac389013eb0d78e75487a25d2c3e14f72
talha bhatti <talha.basharat@ccjk.com>
2019-10-10 17:56:37 +0500

    crud

---
+++ src/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+class UserController extends Controller
+
+{
+
+    public function number(Request $request)
+    {
+
+ return $this->render('');
+    }
+
+}
